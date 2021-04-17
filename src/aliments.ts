f3dc4d48a97416fb997e30625e27a87e7d88d050
Adrian Melian Fernandez <alu0100974652@ull.edu.es>
2021-04-17 12:03:26 +0100

    Terminado la lista de los alimentos

--- src/aliments.ts
+++ src/aliments.ts
@@ -1,5 +1,6 @@
 import {Food} from "./alimento";
 
+
 const Almendrado: Food = new Food('Almendrado',
     'España', [431, 9, 57, 18.1], 5.05, 'Grupo1');
 
@@ -8,6 +9,28 @@ const Pollo: Food = new Food('Pollo',
 
 const CerdoChuleta: Food = new Food('Cerdo chuleta',
     'España', [327, 15.4, 0, 29.5], 6.98, 'Grupo1');
+
+const Atun: Food = new Food('Atun',
+    'España', [200, 23, 0, 12], 10.90, 'Grupo1');
+
+const Salmon: Food = new Food('Salmon',
+    'España', [182, 18.4, 0, 12], 8.99, 'Grupo1');
+
+const HuevoGallina: Food = new Food('Huevo de gallina',
+    'España', [153, 12.5, 0.7, 11.1], 3.19, 'Grupo1');
+
+const Garbanzos: Food = new Food('Garbanzos',
+    'España', [332, 21.3, 49.6, 5.4], 2.05, 'Grupo1');
+
+const Tofu: Food = new Food('Tofu',
+    'España', [74, 8.1, 0.7, 4.2], 6.50, 'Grupo1');
+
+const NuecesConCascara: Food = new Food('Nueces con cascara',
+    'España', [611, 14, 3.3, 59], 6.84, 'Grupo1');
+
+const CocoRallado: Food = new Food('Coco rallado',
+    'España', [373, 3.2, 3.7, 36], 3.29, 'Grupo1');
+
 
 
 const Ajo: Food = new Food('Ajo',
@@ -41,14 +64,65 @@ const Repollo: Food = new Food('Repollo',
     'España', [36, 3.3, 3.4, 0.4], 0.99, 'Grupo2');
 
 
-const ArrozBlanco: Food = new Food('Arroz',
-    'España', [381, 7, 86, 0.9], 0.79, 'Grupo4');
-
-const Avena: Food = new Food('Avena',
-    'España', [379, 13.2, 61.9, 6.52], 2.11, 'Grupo4');
-
-const BasePizza: Food = new Food('Base de pizza',
-    'España', [373, 6.7, 43.9, 7.4], 5, 'Grupo4');
 
 const LecheCabra: Food = new Food('Lecha de cabra',
     'España', [69, 3.4, 4,5, 3.9], 0.64, 'Grupo3');
+
+const Mantequilla: Food = new Food('Mantequilla',
+    'España', [749, 0.6, Tr, 83], 1.7, 'Grupo3');
+
+const FlanHuevo: Food = new Food('Flan de huevo',
+    'España', [127, 4.9, 21.6, 2.3], 2.7, 'Grupo3');
+
+const Nata: Food = new Food('Nata',
+    'España', [448, 1.5, 2, 48.2], 3.45, 'Grupo3');
+
+const QuesoBurgos: Food = new Food('Queso de burgos',
+    'España', [175, 15, 4, 11], 6.95, 'Grupo3');
+
+const YogurLiquidoSabores: Food = new Food('Yogur liquido de sabores',
+    'España', [75, 2.9, 12.8, 1.4], 2.38, 'Grupo3');
+
+const MousseChocolate: Food = new Food('Mousse de chocolate',
+    'España', [190, 5.1, 22.7, 8.8], 2.63, 'Grupo3');
+
+const YogurNaturalEntero: Food = new Food('Yogur entero natural',
+    'España', [57, 3.7, 4.4, 2.7], 1.99, 'Grupo3');
+
+const QuesoGouda: Food = new Food('Queso Gouda',
+    'España', [331, 25.5, Tr, 25.4], 5.35, 'Grupo3');
+
+const Helado: Food = new Food('Helado',
+    'España', [221, 4.5, 25.4, 10.1], 16.12, 'Grupo3');
+
+
+
+const Fresa: Food = new Food('Fresa',
+    'España', [40, 0.7, 7, 0,5], 2.5, 'Grupo4');
+
+const Kiwi: Food = new Food('Kiwi',
+    'España', [55, 1.1, 10.6, 0.5], 2.45, 'Grupo4');
+
+const Limon: Food = new Food('Limon',
+    'España', [44, 0.7, 9, 0.4], 1.61, 'Grupo4');
+
+const Manzana: Food = new Food('Manzana',
+    'España', [53, 0.3, 12, Tr], 1.19, 'Grupo4');
+
+const Melon: Food = new Food('Melon',
+    'España', [28, 0.6, 6, Tr], 2.35, 'Grupo4');
+
+const Naranja: Food = new Food('Naranja',
+    'España', [42, 0.8, 8.6, Tr], 0.8, 'Grupo4');
+
+const Piña: Food = new Food('Piña',
+    'España', [50, 0.5, 11.5, Tr], 0.88, 'Grupo4');
+
+const Platano: Food = new Food('Platano',
+    'España', [94, 1.2, 20, 0.3], 1.99, 'Grupo4');
+
+const Pera: Food = new Food('Pera',
+    'España', [49, 0.4, 10.6, Tr], 1.33, 'Grupo4');
+
+const Membrillo: Food = new Food('Membrillo',
+    'España', [42, 0.4, 6.8, Tr], , 'Grupo4');
