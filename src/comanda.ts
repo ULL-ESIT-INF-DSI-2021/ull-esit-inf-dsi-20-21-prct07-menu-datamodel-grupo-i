54d42ac765328c42dd8f6d5970b98c9826d337d0
Eduardo Reyes <alu0101120584@ull.edu.es>
2021-04-18 11:28:48 +0100

    Actions prueba

--- src/comanda.ts
+++ src/comanda.ts
@@ -1,9 +1,9 @@
-
 import {Plate} from "./plato";
 import {Menu} from "./menu";
+import {Carta} from "./carta";
 
 export class Comanda {
-  constructor(private pedido: Menu | Plate[]) {
+  constructor(private pedido: Menu | Plate[], pivate readonly carta:Carta) {
   }
 
   getPedido(): Menu | Plate[] {
@@ -11,5 +11,8 @@ export class Comanda {
   }
   setPedido(orden: Menu | Plate[]) {
     this.pedido = orden;
+  }
+  writeMenu() {
+    this.
   }
 }
